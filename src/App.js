eae14534326ed3085beedf404f6d2125cd4cde97
Sanchit Gupta <gupta.sanchit90@gmail.com>
2022-06-24 12:50:28 +0530

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React 18</h1>
+        <h3>Accordion View - No Dependency</h3>
+        <a
+          className="App-link"
+          href="https://github.com/guptasanchit90/react-accordion-no-library"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source Code
+        </a>
+      </header>
+
+      <div className="component-container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
